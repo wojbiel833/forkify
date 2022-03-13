60bb8e8733ed88a6f628a2dc0fc251d58bd84a4c
wojbiel833 <wojbiel833@gmail.com>
2022-03-13 19:05:17 +0100

    Deploy project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '665b20bd-e2e8-4091-a6c2-2d859453f92b';
+export const MODAL_CLOSE_SEC = 2.5;
